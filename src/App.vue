331709c4316d72d5a6ec0dc1b584ce0c410a147c
siutin <osiutino@gmail.com>
2019-06-14 23:51:05 +0800

    grid

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Grid/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Grid from './components/Grid.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Grid
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="grid">
+        <div class="item">1</div>
+        <div class="item">2</div>
+        <div class="item">3</div>
+        <div class="item">4</div>
+        <div class="item">5</div>
+        <div class="item">6</div>
+        <div class="item">7</div>
+        <div class="item">8</div>
+        <div class="item">9</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Grid',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+.grid {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.grid > div.item {
+    margin: 0;
+    text-align: center;
+    font-size: 30px;
+    
+}
+
+.grid > div.item:before {
+  content: "";
+  float: left;
+  padding-top: 100%;
+}
+
+/* 
+  ##Device = Tablets, Ipads (portrait)
+  ##Screen = B/w 768px
+*/
+
+@media (min-width: 768px) {
+  
+  .grid > div.item {
+    width: calc(33.3%);  
+  }
+
+  .grid > div.item:nth-child(odd) {
+      background-color: #ffff11;
+  }
+
+  .grid > div.item:nth-child(even) {
+      background-color: #ff11ff;
+  }  
+}
+
+/* 
+  ##Device = Low Resolution Tablets, Mobiles (Landscape)
+  ##Screen = B/w 481px to 767px
+*/
+
+@media (min-width: 481px) and (max-width: 767px) {
+  .grid > div.item {
+      width: calc(50%);
+  }
+
+  .grid > div.item:nth-child(2n+1) {
+      background-color: #ffff11;
+  }
+
+  .grid > div.item:nth-child(2n+2) {
+      background-color: #ff11ff;
+  }  
+}
+
+/* 
+  ##Device = Most of the Smartphones Mobiles (Portrait)
+  ##Screen = B/w 320px to 479px
+*/
+@media (max-width: 480px) {
+  .grid > div.item {
+      width: calc(100%);
+      
+  }
+
+  .grid > div.item:nth-child(odd) {
+      background-color: #ffff11;
+  }
+
+  .grid > div.item:nth-child(even) {
+      background-color: #ff11ff;
+  }  
+}
+
+</style>
