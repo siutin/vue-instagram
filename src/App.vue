0a67d1e21d104a2e1770f3163c472345f1ee8dce
siutin <osiutino@gmail.com>
2019-06-16 23:27:02 +0800

    dialog

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,14 @@
       <Grid>
          <template v-for="i in range(1, 90)">
           <grid-item :id="i" :key="i" v-slot="{ id, width, isReady }">
-            <thumbnail-item v-if="isReady" :id="i" :width="width"></thumbnail-item>
+            <thumbnail-item v-if="isReady" :id="i" :width="width" v-on:click.native="clickOnTumbnail"></thumbnail-item>
           </grid-item>
         </template>
       </Grid>
+    </div>
+    <div id="cover" v-on:click="closeDialog" v-show="isShowDialog"></div>
+    <div id="dialog-container" class="fade" v-on:click="closeDialog" v-show="isShowDialog">
+      <div id="dialog" v-on:click="closeDialog"></div>
     </div>
   </div>
 </template>
@@ -21,15 +25,59 @@ export default {
   components: {
     Grid, GridItem, ThumbnailItem
   },
+  data () {
+    return {
+      isShowDialog: false
+    }
+  },
   methods: {
     range (start, count) {
       return Array(count).fill(start).map((x, y) => x + y)
+    },
+    clickOnTumbnail (e) {
+      console.log("clickOnTumbnail")
+      this.isShowDialog = true
+    },
+    closeDialog(e) {
+      console.log("closeDialog")
+      this.isShowDialog = false
     }
   }
 }
 </script>
 
 <style scoped>
+#cover {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  
+  background-color: black;
+  opacity: 0.2;  
+}
+
+#dialog-container {
+ position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+#dialog {
+  width: 500px;
+  height: 500px;
+  background-color: #eeeeee;
+  margin-left: auto;
+  margin-right: auto;
+}
+
 .grid > div.item {
   text-align: center;
   font-size: 30px;
@@ -44,6 +92,26 @@ export default {
   float: left;
   padding-top: 100%;
 }
+
+.fade {
+  opacity: 0;
+  animation-name: fadein;
+  animation-duration: .2s;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;
+  animation-direction: normal;
+  animation-timing-function: ease-out; 
+}
+
+@keyframes fadein {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
 </style>
 
 <style>
@@ -58,6 +126,10 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+
+    /* retain dialog position */
+    height: 100vh;
+    overflow-y: scroll;
   }
 
   #container {
