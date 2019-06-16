fd30fe284cfc89b0595ec57a8090577889e43e58
siutin <osiutino@gmail.com>
2019-06-16 19:17:07 +0800

    extract components from Grid to App, make Grid generic

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,50 @@
 <template>
   <div id="app">
     <div id="container">
-      <Grid/>   
+      <Grid>
+         <template v-for="i in range(1, 90)">
+          <grid-item :id="i" :key="i" v-slot="{ id, width, isReady }">
+            <thumbnail-item v-if="isReady" :id="i" :width="width"></thumbnail-item>
+          </grid-item>
+        </template>
+      </Grid>
     </div>
   </div>
 </template>
 
 <script>
 import Grid from './components/Grid.vue'
-
+import GridItem from './components/GridItem'
+import ThumbnailItem from './components/ThumbnailItem'
 export default {
   name: 'app',
   components: {
-    Grid
+    Grid, GridItem, ThumbnailItem
+  },
+  methods: {
+    range (start, count) {
+      return Array(count).fill(start).map((x, y) => x + y)
+    }
   }
 }
 </script>
+
+<style scoped>
+.grid > div.item {
+  text-align: center;
+  font-size: 30px;
+  position: relative;
+  margin: 10px;
+  border: 1px solid #aaaaaa;
+  box-sizing: border-box;
+}
+
+.grid > div.item:before {
+  content: "";
+  float: left;
+  padding-top: 100%;
+}
+</style>
 
 <style>
   * {
@@ -36,4 +65,60 @@ export default {
     margin-left: auto;
     margin-right: auto;
   }
+
+  /*
+  ##Device = Tablets, Ipads (portrait)
+  ##Screen = B/w 768px
+*/
+@media (min-width: 768px) {
+
+  .grid > div.item {
+    flex-basis: calc(33.333% - 20px);
+  }
+
+  .grid > div.item:nth-child(odd) {
+      background-color: #eeeeee;
+  }
+
+  .grid > div.item:nth-child(even) {
+      background-color: #eeeeee;
+  }
+}
+
+/*
+  ##Device = Low Resolution Tablets, Mobiles (Landscape)
+  ##Screen = B/w 481px to 767px
+*/
+@media (min-width: 481px) and (max-width: 767px) {
+  .grid > div.item {
+    flex-basis: calc(50% - 20px);
+  }
+
+  .grid > div.item:nth-child(2n+1) {
+      background-color: #eeeeee;
+  }
+
+  .grid > div.item:nth-child(2n+2) {
+      background-color: #eeeeee;
+  }
+}
+
+/*
+  ##Device = Most of the Smartphones Mobiles (Portrait)
+  ##Screen = B/w 320px to 479px
+*/
+@media (max-width: 480px) {
+  .grid > div.item {
+    flex-basis: calc(100% - 20px);
+  }
+
+  .grid > div.item:nth-child(odd) {
+      background-color: #eeeeee;
+  }
+
+  .grid > div.item:nth-child(even) {
+      background-color: #eeeeee;
+  }
+}
+
 </style>
--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,25 +1,14 @@
 <template>
     <div class="grid">
-        <template v-for="i in range(1, 18)" >
-          <grid-item :id="i" :key="i" v-slot="{ id, width, isReady }">
-            <thumbnail-item v-if="isReady" :id="i" :width="width"></thumbnail-item>
-          </grid-item>
-        </template>
+       <slot></slot>
     </div>
 </template>
 
 <script>
-import GridItem from './GridItem'
-import ThumbnailItem from './ThumbnailItem'
+
 
 export default {
-    components: { GridItem, ThumbnailItem },
-    name: 'Grid',
-    methods: {
-      range (start, count) {
-        return Array(count).fill(start).map((x, y) => x + y)
-      }
-    }
+  name: 'Grid'
 }
 </script>
 
@@ -28,75 +17,4 @@ export default {
     display: flex;
     flex-wrap: wrap;
 }
-
-.grid > div.item {
-  text-align: center;
-  font-size: 30px;
-  position: relative;
-  margin: 10px;
-  border: 1px solid #aaaaaa;
-  box-sizing: border-box;
-}
-
-.grid > div.item:before {
-  content: "";
-  float: left;
-  padding-top: 100%;
-}
-
-/* 
-  ##Device = Tablets, Ipads (portrait)
-  ##Screen = B/w 768px
-*/
-@media (min-width: 768px) {
-  
-  .grid > div.item {
-    flex-basis: calc(33.333% - 20px);
-  }
-
-  .grid > div.item:nth-child(odd) {
-      background-color: #eeeeee;
-  }
-
-  .grid > div.item:nth-child(even) {
-      background-color: #eeeeee;
-  }  
-}
-
-/* 
-  ##Device = Low Resolution Tablets, Mobiles (Landscape)
-  ##Screen = B/w 481px to 767px
-*/
-@media (min-width: 481px) and (max-width: 767px) {
-  .grid > div.item {
-    flex-basis: calc(50% - 20px);
-  }
-
-  .grid > div.item:nth-child(2n+1) {
-      background-color: #eeeeee;
-  }
-
-  .grid > div.item:nth-child(2n+2) {
-      background-color: #eeeeee;
-  }  
-}
-
-/* 
-  ##Device = Most of the Smartphones Mobiles (Portrait)
-  ##Screen = B/w 320px to 479px
-*/
-@media (max-width: 480px) {
-  .grid > div.item {
-    flex-basis: calc(100% - 20px);
-  }
-
-  .grid > div.item:nth-child(odd) {
-      background-color: #eeeeee;
-  }
-
-  .grid > div.item:nth-child(even) {
-      background-color: #eeeeee;
-  }  
-}
-
 </style>
