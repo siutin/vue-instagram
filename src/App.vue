555558c3748c5151857adaa16a36d1cd0aeec217
siutin <osiutino@gmail.com>
2019-06-25 23:38:19 +0800

    handled by autoprefixer

--- src/App.vue
+++ src/App.vue
@@ -114,13 +114,8 @@ export default {
    }
 
   .noselect {
-    -webkit-touch-callout: none; /* iOS Safari */
-      -webkit-user-select: none; /* Safari */
-      -khtml-user-select: none; /* Konqueror HTML */
-        -moz-user-select: none; /* Firefox */
-          -ms-user-select: none; /* Internet Explorer/Edge */
-              user-select: none; /* Non-prefixed version, currently
-                                    supported by Chrome and Opera */
+    -webkit-touch-callout: none;
+    user-select: none;
   }
 
   .profile {
